2b1ba3f650d500293fe39f3224fa398e66e2f7c9
mrtulker <mrtulker7@gmail.com>
2021-12-21 14:49:06 +0300

    teslim

---
+++ JS/items.js
@@ -0,0 +1,195 @@
+// illArray illustration
+// desArray design
+// aniArray animation
+//bir önceki itemı kopyala virgül koymayı unutma köşeli parantezin kapanışından sonra
+
+// name kısmına isim
+// bigphotopath kısmına büyük fotoğrafı site dosyalarının içinde attığın yerin yolu
+// placeholderpath kısmına placeholderı site dosyalarının içinde attığın yerin yolu
+// type kısmına "photo" ve "video" yazacaksın
+// video url kısmını köşeli parantez [] içinde yazacaksın linkleri tırnak içinde yazacaksın birden fazla link eklemek için her bir linki tırnak içinde aralarında virgülle köşeli parantezlerin içine yazacaksın örn: ["url1","url2","url3"]
+
+// fotoğraflar için bigphotopathe videolar için videourlye linkler
+
+
+// IKILI EKLEME
+// {
+//    name: "SPORTS_", ISIM
+//    bigPhotoPath: "", BOŞ
+//    placeHolderPath: "assets/content/aniplaceholder/4.jpg", PLACEHOLDER YOLU
+//    type: "both", TİP both olcak küçük harf hepsi
+//    videoURL: "",
+//    urls: [{
+//          type: "video",
+//          url: "https://player.vimeo.com/video/422240350?h=fe81d31e41"
+//       },
+//       {
+//          type: "photo",
+//          url: "assets/content/ill/KURBAĞA KRAL.jpg"
+//       }
+//    ]  urlerler için şema [ {type:tipi,url: urlsi} , {type ve url } ]
+// },
+
+
+// bu gada
+
+
+
+
+
+const illArray = [{
+      name: "KADIRGA",
+      bigPhotoPath: "assets/content/ill/KADIRGA.jpg",
+      placeHolderPath: "assets/content/illplaceholder/1.jpg",
+      type: "photo",
+      videoURL: "",
+   },
+   {
+      name: "KAFA KAFA İÇİNDE",
+      bigPhotoPath: "assets/content/ill/KAFA KAFA İÇİNDE.jpg",
+      placeHolderPath: "assets/content/illplaceholder/2.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "TİMSAH KIZ",
+      bigPhotoPath: "assets/content/ill/TİMSAH KIZ.jpg",
+      placeHolderPath: "assets/content/illplaceholder/3.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "KAYALIK",
+      bigPhotoPath: "assets/content/ill/KAYALIK.jpg",
+      placeHolderPath: "assets/content/illplaceholder/4.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "BOKSÖR",
+      bigPhotoPath: "assets/content/ill/BOKSÖR.jpg",
+      placeHolderPath: "assets/content/illplaceholder/5.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "DİLLİ BEYİNLİ",
+      bigPhotoPath: "assets/content/ill/DİLLİ BEYİNLİ.jpg",
+      placeHolderPath: "assets/content/illplaceholder/6.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "KARANLIK SİSLİ BİR ORMAN GECESİ",
+      bigPhotoPath: "assets/content/ill/KARANLIK SİSLİ BİR ORMAN GECESİ.jpg",
+      placeHolderPath: "assets/content/illplaceholder/7.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "UZAY KARANLIĞI",
+      bigPhotoPath: "assets/content/ill/UZAY KARANLIĞI.jpg",
+      placeHolderPath: "assets/content/illplaceholder/8.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "VAHŞİ ÇİÇEK",
+      bigPhotoPath: "assets/content/ill/VAHŞİ ÇİÇEK.jpg",
+      placeHolderPath: "assets/content/illplaceholder/9.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "SU ALTI",
+      bigPhotoPath: "assets/content/ill/SU ALTI.jpg",
+      placeHolderPath: "assets/content/illplaceholder/10.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "BATAKLIK",
+      bigPhotoPath: "assets/content/ill/BATAKLIK.jpg",
+      placeHolderPath: "assets/content/illplaceholder/11.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "EJDERHA",
+      bigPhotoPath: "assets/content/ill/EJDERHA.jpg",
+      placeHolderPath: "assets/content/illplaceholder/12.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "KAYA ADAMLAR",
+      bigPhotoPath: "assets/content/ill/KAYA ADAMLAR.jpg",
+      placeHolderPath: "assets/content/illplaceholder/13.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "TAŞ TAŞIYAN",
+      bigPhotoPath: "assets/content/ill/TAŞ TAŞIYAN.jpg",
+      placeHolderPath: "assets/content/illplaceholder/14.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "TELEVİZYON",
+      bigPhotoPath: "assets/content/ill/TELEVİZYON.jpg",
+      placeHolderPath: "assets/content/illplaceholder/15.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "BEBEK",
+      bigPhotoPath: "assets/content/ill/BEBEK.jpg",
+      placeHolderPath: "assets/content/illplaceholder/16.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "KURBAĞA KRAL",
+      bigPhotoPath: "assets/content/ill/KURBAĞA KRAL.jpg",
+      placeHolderPath: "assets/content/illplaceholder/17.jpg",
+      type: "photo",
+      videoURL: "",
+   }, {
+      name: "DENİZ FENERİ",
+      bigPhotoPath: "assets/content/ill/DENİZ FENERİ.jpg",
+      placeHolderPath: "assets/content/illplaceholder/18.jpg",
+      type: "photo",
+      videoURL: "",
+   },
+];
+const desArray = [{
+      name: "STORYTEL MOTION GRAPHIC",
+      bigPhotoPath: "",
+      placeHolderPath: "assets/content/aniplaceholder/5.jpg",
+      type: "video",
+      videoURL: ["https://player.vimeo.com/video/656051609?h=7d57e2e5a8"],
+
+   },
+   {
+      name: "OPENEYE",
+      bigPhotoPath: "assets/content/des/OPENEYE.jpg",
+      placeHolderPath: "assets/content/desplaceholder/1.jpg",
+      type: "photo",
+      videoURL:""
+   },
+];
+const aniArray = [{
+      name: "OPENEYE TRAILER",
+      bigPhotoPath: "",
+      placeHolderPath: "assets/content/aniplaceholder/1.jpg",
+      type: "video",
+      videoURL: ["https://player.vimeo.com/video/490751806?h=4f0be92a52"],
+   },
+   {
+      name: "OPENEYE MOTION POSTER",
+      bigPhotoPath: "",
+      placeHolderPath: "assets/content/aniplaceholder/1.jpg",
+      type: "video",
+      videoURL: ["https://player.vimeo.com/video/638860635?h=0cfb99b9fe"],
+   },
+   {
+      name: "FISH",
+      bigPhotoPath: "",
+      placeHolderPath: "assets/content/aniplaceholder/3.jpg",
+      type: "video",
+      videoURL: ["https://player.vimeo.com/video/411335446?h=db099e0e93"],
+   },
+   {
+      name: "SPORTS",
+      bigPhotoPath: "",
+      placeHolderPath: "assets/content/aniplaceholder/4.jpg",
+      type: "video",
+      videoURL: ["https://player.vimeo.com/video/422240350?h=fe81d31e41"],
+   },
+];
---
+++ JS/main.js
@@ -0,0 +1,217 @@
+function wait(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var isAnimating = false;
+var isBigPhotoOpened = false;
+async function categoryOpen(e) {
+   if (isAnimating == false) {
+      isAnimating = true;
+      let array = $(".item").toArray();
+      let id = $(e).attr("id");
+      let category = $(e).attr("data-category");
+      $($(e).children(".item-cover")).css("background-color", "#ffffffcc");
+      array.forEach(el => {
+         if ($(el).attr("id") != id) {
+            $(el).animate({
+               top: "-100%"
+            }, 700)
+         } else {
+            selected = el;
+         }
+      });
+      await wait(700);
+      $(selected).animate({
+         left: "0%"
+      }, 400);
+      await wait(300);
+      $(".contact-container").css("display", "flex");
+      $(selected).toggleClass("item-selected");
+      contentCreater(category);
+      $(selected).removeAttr("onclick");
+      $(selected).attr("onclick", "categoryClose(this)");
+      await wait(300);
+      $(".contact-container").toggleClass("contact-opened");
+      $(".content-container").toggleClass("content-opened");
+      await wait(1000);
+      isAnimating = false;
+   }
+}
+async function categoryClose(e) {
+   if (isAnimating == false) {
+      isAnimating = true;
+      if (isBigPhotoOpened == true) {
+         bigPhotoClose();
+         await wait(300);
+      }
+      $("body").animate({
+         top: "-100vh"
+      });
+      await wait(400);
+      $(".contact-container").css("display", "none");
+      $(".contact-container").css("opacity", "0");
+      $(".contact-container").toggleClass("contact-opened");
+      $(".content-container").toggleClass("content-opened");
+      $(e).attr("onclick", "categoryOpen(this)");
+      $("item-selected").toggleClass("item-fast-close");
+      $(".item-selected").toggleClass("item-selected");
+      $(".item").removeAttr("style");
+      $(".item-cover").removeAttr("style");
+      await wait(300)
+      $("body").animate({
+         top: "0vh"
+      });
+      $(".content-container").html("");
+      await wait(400);
+      $(".contact-container").css("opacity", "1");
+      isAnimating = false;
+   }
+}
+
+function contentCreater(id) {
+   let arr;
+   switch (id) {
+      case "illustration":
+         arr = illArray;
+         _category = "illustration";
+         break;
+      case "design":
+         arr = desArray;
+         _category = "design";
+         break;
+      case "animation":
+         arr = aniArray;
+         _category = "animation";
+         break;
+      default:
+         break;
+   }
+   for (let i = 0; i < arr.length; i++) {
+      item = `<div class="content-item">
+      <div class="content-item-wrapper" style="background-image: url(${arr[i].placeHolderPath});">
+         <div class="content-item-cover" onclick="bigPhotoOpener(this)" data-link="${arr[i].bigPhotoPath}" data-name="${arr[i].name}" data-type="${arr[i].type}" data-videolink="${arr[i].videoURL}" data-bothURL="${arr[i].urls}" data-category="${_category}">
+            <p>${arr[i].name}</p>
+         </div>
+      </div>
+   </div>`
+      $(".content-container").append(item);
+   }
+}
+
+async function bigPhotoOpener(item) {
+   isBigPhotoOpened = true;
+   let link = $(item).attr("data-link");
+   let name = $(item).attr("data-name");
+   let type = $(item).attr("data-type");
+   if (type == "photo") {
+      v_arr = $(item).attr("data-link").split(",");
+      let html = `<div class="photo-container">
+      <img src="assets/misc/cross.svg" alt="" class="cross" onclick="bigPhotoClose()">
+      <div class="photo-header">
+         <h1>${name}</h1>
+      </div>`
+      for (let i = 0; i < v_arr.length; i++) {
+         html += ` 
+         <div class="photo-container-img-container">
+            <img src="${v_arr[i]}" alt="${name}" id="img">
+         </div>`
+      }
+      html += `</div>`
+      $("body").append(html);
+   } else if (type == "video") {
+
+
+      v_arr = $(item).attr("data-videolink").split(",");
+      let html = `<div class="photo-container">
+      <img src="assets/misc/cross.svg" alt="" class="cross" onclick="bigPhotoClose()">
+      <div class="photo-header">
+         <h1>${name}</h1>
+      </div>`
+      for (let i = 0; i < v_arr.length; i++) {
+         html += `<div class="video-container"><iframe src="${v_arr[i]}8&color=ffffff&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>`
+      }
+      html += `</div>`
+      $("body").append(html);
+   } else if (type == "both") {
+      let category = $(item).attr("data-category");
+      let searchArr;
+      switch (category) {
+         case "illustration":
+            searchArr = illArray;
+            break;
+         case "design":
+            searchArr = desArray;
+            break;
+         case "animation":
+            searchArr = aniArray;
+            break;
+         default:
+            console.error("BEKLENMEDİK BİR HATA İLE KARŞILAKTIK");
+            break;
+      }
+
+      var result = searchArr.filter(obj => {
+         return obj.name == name
+      })
+      let bothArray = result[0].urls;
+      let html = `<div class="photo-container">
+      <img src="assets/misc/cross.svg" alt="" class="cross" onclick="bigPhotoClose()">
+      <div class="photo-header">
+         <h1>${name}</h1>
+      </div>`
+      for (let i = 0; i < bothArray.length; i++) {
+         if (bothArray[i].type == "photo") {
+            html += ` 
+            <div class="photo-container-img-container">
+               <img src="${bothArray[i].url}" alt="${name}" id="img">
+            </div>`
+         } else {
+            html += `<div class="video-container"><iframe src="${bothArray[i].url}8&color=ffffff&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>`
+         }
+      }
+      html += `</div>`
+      $("body").append(html);
+   }
+   await wait(100);
+   $(".photo-container").animate({
+      opacity: "1"
+   }, 300);
+}
+
+async function bigPhotoClose() {
+   $(".photo-container").animate({
+      opacity: "0"
+   }, 300);
+   await wait(300);
+   $(".photo-container").remove();
+   isBigPhotoOpened = false;
+}
+
+$('body').on('click', '.photo-container', function (e) {
+   if ($(e.target).attr("id") != "img") {
+      bigPhotoClose();
+   }
+});
+
+async function videoCloser() {
+   $(".entry-video-container").animate({
+      top: "-100vh"
+   }, 500);
+   await wait(500);
+   $(".entry-video-container").remove();
+}
+var video = document.getElementById("entryvideo");
+video.onended = async function () {
+   await wait(1000);
+   videoCloser();
+}
+
+$("body").css("display", "block");
+
+var logArr = [`console.log('%c Bu siteyi yaptıran adam sitenin bilgisayardan kullanılmasını istiyor ', 'background: #000; color: #fff');`, `
+console.log('%c Bu siteyi yaptıran adam sitenin kaynağından fotoğrafların alınmasını istemiyor ', 'background: #f00; color: #fff');`, `
+console.log('%c Bu siteyi yapan adama @ysrkaanky instagram adresinden veya ysrkaanky@gmail.com adresinden ulaşabilirsiniz  ', 'background: #00f; color: #fff');`, `console.log('%c İyi İncelemeler =)  ', 'background: #070; color: #fff');`];
+
+for (let i = 0; i < logArr.length; i++) {
+   eval(logArr[i]);
+}
